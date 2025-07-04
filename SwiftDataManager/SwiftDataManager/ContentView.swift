ed92d4335d8da256b3e8b16fbd122e5e87460d8e
Arsenty Halimovsky <ghost12@yandex.ru>
2025-07-04 14:56:13 +0300

    Instantiate a new project

---
+++ SwiftDataManager/SwiftDataManager/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftDataManager
+//
+//  Created by Арсентий Халимовский on 04.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ SwiftDataManager/SwiftDataManager/SwiftDataManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataManagerApp.swift
+//  SwiftDataManager
+//
+//  Created by Арсентий Халимовский on 04.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
