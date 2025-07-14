18b3f75ed3841785c380f8e0cf67fb9eb55752aa
Arsenty Halimovsky <ghost12@yandex.ru>
2025-07-14 10:19:19 +0300

    Create CompositionRoot

--- SwiftDataManager/SwiftDataManager/AppFoundation/CompositionRoot.swift
+++ SwiftDataManager/SwiftDataManager/AppFoundation/CompositionRoot.swift
@@ -1,1 +1,34 @@
-import Foundation
+import SwiftData
+
+@MainActor
+final class CompositionRoot {
+	
+	var modelContainer: ModelContainer
+	var swiftDataManager: SwiftDataManager
+	
+	init() {
+		
+		do {
+			self.modelContainer = try ModelContainer.init(for: MainStateSnapshot.self)
+			
+			self.swiftDataManager = SwiftDataManager(context: self.modelContainer.mainContext)
+			print("CompositionRoot init complete")
+			
+		} catch {
+			fatalError("Failed to Construct CompositionRoot")
+		}
+		
+	}
+	
+	@MainActor
+	func buildMainView() -> MainView {
+		
+		let mainStateSnapshot = self.swiftDataManager.loadMainStateSnapshot() ?? MainStateSnapshot(items: 1)
+		
+		let viewModel = MainViewModel(swiftDataManager: self.swiftDataManager,
+									  mainStateSnapshot: mainStateSnapshot)
+		let view = MainView(viewModel: viewModel)
+		print("MainView has been made")
+		return view
+	}
+}
